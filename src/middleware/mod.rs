c5e6f4c8c08757379cb3a82871c4cc68b49be746
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2025-02-07 11:11:55 +0900

    feat: 미들웨어 모듈에 문서 주석 추가 및 재사용 가능한 타입 별칭 정의

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -1,9 +1,16 @@
-pub mod chain;
-pub mod config;
-pub mod error;
-pub mod traits;
+//! 미들웨어 프레임워크 모듈
+//! 
+//! HTTP 요청/응답을 처리하는 미들웨어 체인을 구현합니다.
+
+mod chain;
+mod error;
+mod traits;
 
 pub use chain::MiddlewareChain;
-pub use config::MiddlewareConfig;
 pub use error::MiddlewareError;
 pub use traits::Middleware;
+
+// 재사용 가능한 타입 별칭
+pub type Request = hyper::Request<hyper::body::Incoming>;
+pub type Response = hyper::Response<http_body_util::Full<bytes::Bytes>>;
+pub type BoxError = Box<dyn std::error::Error + Send + Sync>;
