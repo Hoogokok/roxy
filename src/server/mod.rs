d7e07be0903ae9d79340a3943bb9c0ab41df2338
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2025-02-13 12:11:16 +0900

    feat: 서버 모듈에서 에러 처리 모듈 추가 및 공개 모듈 정리

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,8 +1,11 @@
+pub mod handler;
+pub mod listener;
+pub mod docker;
+pub mod error;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
 mod manager;
-mod listener;
-mod handler;
-mod docker;
 
-pub use manager::ServerManager;
-
-pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>; 
+use error::Error;
+pub use manager::ServerManager; 
