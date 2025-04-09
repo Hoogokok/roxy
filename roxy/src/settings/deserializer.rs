b20c82419ee37111d78b36166c88060534b7f527
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2025-04-09 15:30:34 +0900

    feat(settings): LoadBalancerSettings 제거 및 서비스 설정을 위한 HashMap 추가

--- roxy/src/settings/deserializer.rs
+++ roxy/src/settings/deserializer.rs
@@ -3,7 +3,7 @@ use std::marker::PhantomData;
 use serde::Deserialize;
 use serde::de::Error;
 
-use crate::settings::core::{AnyLoadBalancerSettings, Settings};
+use crate::settings::core::Settings;
 use crate::settings::server::HttpsDisabled;
 use crate::settings::typestate::{Raw, Validated};
 use crate::settings::logging::LogSettings;
@@ -11,7 +11,6 @@ use crate::settings::tls::TlsSettings;
 use crate::settings::docker::DockerSettings;
 use crate::middleware::config::MiddlewareConfig;
 use crate::settings::types::ValidMiddlewareId;
-use crate::settings::load_balancer::{LoadBalancerSettings, NoLoadBalancing};
 
 /// Settings 역직렬화 구현
 impl<'de> Deserialize<'de> for Settings<Validated, HttpsDisabled> {
@@ -60,7 +59,7 @@ impl<'de> Deserialize<'de> for Settings<Validated, HttpsDisabled> {
             docker: validated_docker,
             middleware: settings_helper.middleware,
             router_middlewares: settings_helper.router_middlewares,
-            load_balancer: AnyLoadBalancerSettings::None(LoadBalancerSettings::<Validated, NoLoadBalancing>::new()),
+            services: HashMap::new(),
             _marker: PhantomData,
         })
     }
